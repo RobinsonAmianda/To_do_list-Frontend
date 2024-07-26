65c36d879e427c6d7e32f7c768af3d9d3099a25b
festus-sulumeti <sulumetifestus@outlook.com>
2024-07-26 16:04:10 +0300

    the changes

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -10,7 +10,7 @@ const WelcomePage = () => {
       <div className="container">
         <h1 className="heading">Welcome to task manager</h1>
         <br />
-        <p className="description">Get tasks done quicker in a well managed andorganised manner</p>
+        <p className="description">Get tasks done quicker in a well managed and organised manner</p>
         <img src={welcome} alt="Office Snapshot" className="office-image" />
         <Link to="/login" className="link login-link">Login</Link>
         <Link to="/signup" className="link signup-link">Create account</Link>
