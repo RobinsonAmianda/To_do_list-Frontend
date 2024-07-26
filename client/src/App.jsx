f67423b5d20c2bd8d66f0a928ddaaeff10c1b1b7
festus-sulumeti <sulumetifestus@outlook.com>
2024-07-26 15:37:27 +0300

    the welcomepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Home from './components/Home';
+import WelcomePage from './components/WelcomePage';
 
 function App() {
   return (
-      <div className="App">
+      <div>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element ={<WelcomePage />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/home" element={<Home />} />
         </Routes>
