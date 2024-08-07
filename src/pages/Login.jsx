797adf2241b560c2d06345472ff0e66c7083c57a
festus-sulumeti <sulumetifestus@outlook.com>
2024-08-07 11:10:09 +0300

    the project

---
+++ src/pages/Login.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import '../Styling/pages/Login.css';
+import { ToastContainer, toast } from "react-toastify";
+import { Link, useNavigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+
+const Userlogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("backendURL", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        toast.success("Successfully signed in", {
+          autoClose: 100,
+          onClose: () => {
+          navigate ( "/Userhomepage" );
+          },
+        });
+      } else {
+        toast.error("Invalid email or password");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      toast.error("An error occurred. Please try again.");
+    }
+  };
+
+  console.log("Userlogin component rendered");
+
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <h2>Sign In</h2>
+       
+        <form onSubmit={handleSignIn}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Sign In</button>
+        </form>
+        <br />
+        <p>No account? <Link to="/signup">Signup</Link></p>
+        <ToastContainer />
+      </div>
+    </div>
+  );
+};
+
+export default Userlogin;
